6bbd041ad4bc2ec65f132e69187ee889ee538af2
parksb <parkgds@gmail.com>
2024-06-01 12:12:13 +0900

    implement all requirements

--- lab-7/Assets/Scripts/WoodenPlankController.cs
+++ lab-7/Assets/Scripts/WoodenPlankController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class WoodenPlankController : MonoBehaviour
 {
+    private AudioSource audiosrc;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audiosrc = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Weapon"))
+        {
+            audiosrc.Play();
+            GetComponent<Renderer>().enabled = false;
+            Destroy(gameObject, 2);
+        }
     }
 }
